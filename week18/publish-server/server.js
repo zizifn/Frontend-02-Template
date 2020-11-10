7cb386606a8c168091e84501bbb9bd73d977ff4a
zizifn <515868058@qq.com>
2020-11-10 20:45:17 +0800

    update week18

--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -1,12 +1,29 @@
 let http = require('http')
+let fs = require('fs')
 
 
 
 http.createServer(
     (req, res) => {
-        console.log(req);
+        console.log(req.headers);
+        const outFile = fs.createWriteStream('./public/index.html')
+        req.pipe(outFile);
 
-        res.end('hello world!')
+        // const outFile = fs.createWriteStream('./public/index.html')
+        // req.on('data', (chunk) => {
+        //     console.log(chunk.toString());
+        //     outFile.write(chunk);
+        // })
+
+        // req.on('end', () => {
+        //     outFile.end()
+        //     res.end('success')
+        // })
+
+        req.on('end', () => {
+            res.end('success')
+        })
+
 
     }
 ).listen(8082)
--- week18/publish-tool/publish.js
+++ week18/publish-tool/publish.js
@@ -1,12 +1,36 @@
 let http = require('http')
+let fs = require('fs');
+const { post } = require('../server/routes');
+
 
 let request = http.request(
     {
         hostname: '127.0.0.1',
-        port: 8082
+        port: 8082,
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        }
     }, res => {
         console.log(res);
     }
 );
 
-request.end();
+let file = fs.createReadStream("./index.html");
+
+file.pipe(request);
+// let file = fs.createReadStream("./index.html");
+// // 如果文件过大，event 'data'， 会调用多次
+// // let file = fs.createReadStream(".pdf");
+
+// file.on('data', chunk => {
+//     // console.log(chunk.toString());
+//     request.write(chunk);
+// })
+
+// file.on('end', () => {
+//     console.log('end finshed');
+//     request.end();
+// })
+
+// // request.end();
