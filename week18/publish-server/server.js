6b5c994e075e63b5c75cbd8162e0c44c32859b03
zizifn <515868058@qq.com>
2020-11-29 19:13:52 +0800

    chore: update 总结

--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -1,17 +1,34 @@
 let http = require('http')
 let fs = require('fs')
 let unzipper = require('unzipper')
+let queryString = require('querystring')
 
-
+// auth 路由
+/**
+ *
+ * @param {IncomingMessage} req
+ * @param {*} res
+ */
+function auth(req, res) {
+    let query = queryString.parse(req.url)
+    console.log(query);
+}
 
 http.createServer(
     (req, res) => {
         console.log(req.headers);
+        if (req.url.includes('/auth')) {
+            auth(req, res);
+        }
+        //
+
+
+
         // const outFile = fs.createWriteStream('./public/build.zip')
         // req.pipe(outFile);
-        req.pipe(unzipper.Extract({ path: './public/build/' })).on('error', () => {
-            console.log("error");
-        })
+        // req.pipe(unzipper.Extract({ path: './public/build/' })).on('error', () => {
+        //     console.log("error");
+        // })
 
         // const outFile = fs.createWriteStream('./public/index.html')
         // req.on('data', (chunk) => {
@@ -19,12 +36,12 @@ http.createServer(
         //     outFile.write(chunk);
         // })
 
-        req.on('end', () => {
-            // outFile.end()
-            console.log("end");
-            res.writeHead(200, { "content-type": "text/html" });
-            res.end('success')
-        })
+        // req.on('end', () => {
+        //     // outFile.end()
+        //     console.log("end");
+        //     res.writeHead(200, { "content-type": "text/html" });
+        //     res.end('success')
+        // })
 
         // outFile.on('error', function (err) {
         //     console.log(err);
--- week18/publish-tool/publish.js
+++ week18/publish-tool/publish.js
@@ -2,53 +2,63 @@ let http = require('http')
 let fs = require('fs');
 let archiver = require('archiver')
 
-const archive = archiver('zip', {
-    zlib: { level: 9 }
-});
-
-archive.directory('dist/', false);
-// archive.pipe(fs.createWriteStream("build.zip"));
+let child_process = require("child_process")
 
 
+// open https://github.com/login/oauth/authorize?client_id= Iv1.fae9105c0e565b1a
+
+child_process.exec("start https://github.com/login/oauth/authorize?client_id=Iv1.fae9105c0e565b1a")
+// http.createServer((req, res) => {
+
+// })
+
+// const archive = archiver('zip', {
+//     zlib: { level: 9 }
+// });
+
+// archive.directory('dist/', false);
+// // archive.pipe(fs.createWriteStream("build.zip"));
 
 
 
-let request = http.request(
-    {
-        hostname: '127.0.0.1',
-        port: 8082,
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/octet-stream',
-            // 'Content-Length': 1235
-            'Transfer-Encoding': 'chunked'
-        }
-    }, res => {
-        console.log(res.statusCode);
-        res.on("data", (chunk) => {
-            console.log(chunk.toString());
-        })
 
-        res.on("error", (chunk) => {
-            console.log(chunk.toString());
-        })
 
-    }
-);
+// let request = http.request(
+//     {
+//         hostname: '127.0.0.1',
+//         port: 8082,
+//         method: 'post',
+//         headers: {
+//             'Content-Type': 'application/octet-stream',
+//             // 'Content-Length': 1235
+//             'Transfer-Encoding': 'chunked'
+//         }
+//     }, res => {
+//         console.log(res.statusCode);
+//         res.on("data", (chunk) => {
+//             console.log(chunk.toString());
+//         })
 
-// let file = fs.createReadStream("build.zip");
-archive.pipe(request);
-request.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-});
+//         res.on("error", (chunk) => {
+//             console.log(chunk.toString());
+//         })
 
-archive.on('end', () => {
-    request.end();
-})
-archive.finalize();
-    // fs.stat("build.zip", (error, stat) => {
+//     }
+// );
 
-    // });
+// // let file = fs.createReadStream("build.zip");
+// archive.pipe(request);
+// request.on('error', (e) => {
+//     console.error(`problem with request: ${e.message}`);
+// });
+
+// archive.on('end', () => {
+//     request.end();
+// })
+// archive.finalize();
+//     // fs.stat("build.zip", (error, stat) => {
+
+//     // });
 
 
 
